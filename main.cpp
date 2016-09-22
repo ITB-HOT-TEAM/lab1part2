85933142dfa074f328d9c6b7d9902c69d5513050
Kaspar Siricenko <B00076450@student.itb.ie>
2016-09-22 12:36:42 +0100

    Deleting unnecessery files and modifying main.cpp

--- main.cpp
+++ main.cpp
@@ -23,17 +23,49 @@ class Triangle: public Shape
     int getArea() { return (width * height)/2; }
 };
 
+class Circle: public Shape
+{
+    public:
+
+    int getArea() {
+        if(width != height){
+            cerr << "Data corrupted: Circle object has different values for width and height" << endl;
+            throw std::exception();
+        }
+
+        return width*height*pi;
+    }
+
+    void  setWidth(int width) { this->width = width; this->height = width; }
+    void  setHeight(int height) { this->width = height; this->height = height; }
+
+    private:
+        const double pi = 3.141592659;
+
+};
 
 //Main
 int main(void)
 {
-    Triangle Tri;
+    Triangle tri;
+    Rectangle rect;
+    Circle circ;
 
-    Tri.setWidth(5);
-    Tri.setHeight(7);
+
+    rect.setWidth(5);
+    rect.setHeight(7);
+
+    circ.setWidth(5);
+    circ.setHeight(7);
+
+    tri.setWidth(5);
+    tri.setHeight(7);
 
     // Print the area of the object.
-    cout << "Total Triangle area: " << Tri.getArea() << endl;
+    cout << "Total Rectangle area: " << rect.getArea() << endl;
+    cout << "Total Circle area: " << circ.getArea() << endl;
+    cout << "Total Triangle area: " << tri.getArea() << endl;
+    cout << "Total of all objects area: " << rect.getArea()+circ.getArea()+tri.getArea() << endl;
 
     return 0;
 }
